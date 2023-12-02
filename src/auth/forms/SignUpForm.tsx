c2479149ebc77eabf921162c87662e38a7a6d4b0
GiorgiKalatozi <giorgikalatoz@gmail.com>
2023-12-03 03:08:14 +0400

    feat: form component

--- src/auth/forms/SignUpForm.tsx
+++ src/auth/forms/SignUpForm.tsx
@@ -1,5 +1,33 @@
-function SignUpForm() {
-  return <div>SignUpForm</div>;
-}
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-export default SignUpForm;
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import * as z from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+export default function SignUpForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
