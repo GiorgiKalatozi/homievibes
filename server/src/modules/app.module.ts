89ad7dbd6af2485e486ebe9083d81f24b26ea5a6
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-01-21 22:27:44 +0400

    feat: add migrations'

--- server/src/modules/app.module.ts
+++ server/src/modules/app.module.ts
@@ -7,9 +7,23 @@ import { CommentsModule } from './comments/comment.module';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
 import { PostsModule } from './posts/posts.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeorm from '../config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [AuthModule, UsersModule, PostsModule, CaslModule, CommentsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [typeorm] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    CaslModule,
+    CommentsModule,
+  ],
   controllers: [],
   providers: [
     UsersService,
