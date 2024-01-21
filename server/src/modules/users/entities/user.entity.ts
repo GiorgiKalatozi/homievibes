408095c7c3f888a9896313493806239d39396cd5
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-01-21 20:00:59 +0400

    feat: add user entity

--- server/src/modules/users/entities/user.entity.ts
+++ server/src/modules/users/entities/user.entity.ts
@@ -1,7 +1,19 @@
-import { Role } from 'src/common/enums';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class User {
-  id: number;
-  users: Role[];
-  isAdmin: boolean;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ unique: true, nullable: false })
+  username: string;
+
+  @Column({ unique: true, nullable: false })
+  email: string;
+
+  @Column()
+  password: string;
 }
