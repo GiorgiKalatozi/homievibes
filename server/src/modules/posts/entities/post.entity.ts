59ec063477178fdd2f5b0b67c07cafbf059bde01
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-01-21 22:37:06 +0400

    feat: add post entity and relationships

--- server/src/modules/posts/entities/post.entity.ts
+++ server/src/modules/posts/entities/post.entity.ts
@@ -1,10 +1,37 @@
 import { User } from 'src/modules/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
+@Entity()
 export class Post {
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
   title: string;
+
+  @Column()
   content: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  @JoinColumn({ name: 'author_id' })
   author: User;
+
+  @Column({ name: 'author_id', nullable: false })
+  authorId: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
   updatedAt: Date;
 }
