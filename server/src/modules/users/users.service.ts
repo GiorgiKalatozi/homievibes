ebce945a0aeb00cfafcc80a6fe550f6081323c2b
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-01-21 23:14:22 +0400

    feat: add user crud

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  public async create(user: User): Promise<User> {
+    const newUser = this.usersRepository.create(user);
+    return await this.usersRepository.save(newUser);
+  }
+
+  public async findAll(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  public async findOne(id: string): Promise<User> {
+    return await this.usersRepository.findOne({ where: { id } });
+  }
+
+  public async update(id: string, user: User): Promise<User> {
+    const userToUpdate = await this.usersRepository.findOne({ where: { id } });
+    if (!userToUpdate) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    await this.usersRepository.update(id, user);
+    return userToUpdate;
+  }
+
+  public async remove(id: string): Promise<void> {
+    const result = await this.usersRepository.delete({ id });
+
+    if (result.affected === 0) {
+      throw new NotFoundException('User not found');
+    }
+  }
+}
