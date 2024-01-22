5d7550a678c8b02c552e41bfd92087eca879d9da
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-01-22 13:57:23 +0400

    chore: local strategy

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersService } from '../users/users.service';
 import { JwtModule, JwtService } from '@nestjs/jwt';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  controllers: [AuthController, UsersService, JwtService],
+  controllers: [AuthController, UsersService, JwtService, LocalStrategy],
   providers: [AuthService],
   imports: [
     JwtModule.register({
